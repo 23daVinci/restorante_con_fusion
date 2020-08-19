2fb1aaa0e18851eee96a2f79ec65effbe292a878
da Vinci <namanlazarus02@gmail.com>
2020-08-19 16:24:14 +0530

    Uncontrolled form

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,23 +1,42 @@
 import React, { Component } from 'react'
-import { Navbar, NavbarBrand, Nav, NavbarToggler, NavItem, Collapse, Jumbotron } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, NavItem, Collapse, Jumbotron, Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+import Form from 'reactstrap/lib/Form';
+import FormGroup from 'reactstrap/lib/FormGroup';
+import Label from 'reactstrap/lib/Label';
+import Input from 'reactstrap/lib/Input';
 
 class NavbarComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
             restaurantName: "Restorante Con Funsion",
-            isNavOpen: false
+            isNavOpen: false,
+            isModalOpen: false
         };
 
         // binding
         this.ToggelNav = this.ToggelNav.bind(this);
+        this.ToggelModal = this.ToggelModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
     }
 
     ToggelNav() {
         this.setState({
             isNavOpen: !this.state.isNavOpen
         });
+    }
+
+    ToggelModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleLogin(event) {
+        this.ToggelModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value + " Remember: " + this.remember.checked);
+        event.preventDefault(); 
     }
 
     render() {
@@ -55,6 +74,14 @@ class NavbarComponent extends Component {
                                     </NavLink>
                                 </NavItem>
                             </Nav>
+
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.ToggelModal}>
+                                        <span className="fa fa-sign-in fa-lg"></span> Login
+                                    </Button>
+                                </NavItem>
+                            </Nav>
                         </Collapse>
                     </div>
                 </Navbar>
@@ -66,6 +93,32 @@ class NavbarComponent extends Component {
                         </div>
                     </div>
                 </Jumbotron>
+
+                <Modal isOpen={ this.state.isModalOpen } toggle={ this.ToggelModal }>
+                    <ModalHeader toggle={ this.ToggelModal }>Login</ModalHeader>
+                    <ModalBody>
+                    <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input}  />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                    innerRef={(input) => this.remember = input}  />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
             </React.Fragment>
         );
     }
